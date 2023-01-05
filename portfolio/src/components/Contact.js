8a37f47968c7cd06edfb1ca3f89c58fbfbf6ad07
HaiPhham <haiphamtrung62@gmail.com>
2023-01-05 20:58:08 +0700

    update

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Contact.css';
 import { FaAward } from 'react-icons/fa';
 import { FaUserFriends } from 'react-icons/fa';
 import { ImFolderOpen } from 'react-icons/im';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_5sguktu',
+        'template_100ugrj',
+        form.current,
+        'eyBSlfuCqJEXgrE5e'
+      )
+      .then(
+        (result) => {
+          console.log(result.name);
+          var element = document.getElementById('mail-form');
+          element.reset();
+        },
+        (error) => {
+          console.log(error.name);
+        }
+      );
+  };
   const icons = [
-    { icon: <FaAward />, title: 'Experience', text: '20+ Years Working' },
-    { icon: <FaUserFriends />, title: 'Clients', text: 'Over 100 Worldwide' },
+    {
+      icon: <FaAward />,
+      platform: 'Experience',
+      name: 'haiphamtrung62@gmail.com',
+      link: 'mailto:haiphamtrung62@gmail.com',
+    },
+    {
+      icon: <FaUserFriends />,
+      platform: 'Clients',
+      name: 'Phạm Trung Hải',
+      link: 'https://m.me/qhai62',
+    },
     {
       icon: <ImFolderOpen />,
-      title: 'Projects',
-      text: '100 Completed Projects',
+      platform: 'Zalo',
+      name: '0903349317',
+      link: 'https://zalo.me/84903349317',
     },
   ];
   return (
@@ -23,22 +58,24 @@ function Contact() {
         <div className="contact-med">
           {icons.map((icon) => {
             return (
-              <div className="contact-box" key={icon.text}>
+              <div className="contact-box" key={icon.name}>
                 <article>
                   {icon.icon}
-                  <h3>{icon.title}</h3>
-                  <h5>{icon.text}</h5>
+                  <h3>{icon.platform}</h3>
+                  <h5>{icon.name}</h5>
+                  <a href={icon.link}>Send a message</a>
                 </article>
               </div>
             );
           })}
         </div>
         <div className="mailing-form">
-          <form>
+          <form ref={form} onSubmit={sendEmail} id="mail-form">
             <div className="input-container">
               <input
                 className="input"
-                type="text"
+                type="name"
+                name="name"
                 placeholder="What's your name?"
                 required
               />
@@ -47,16 +84,20 @@ function Contact() {
             <input
               className="input"
               type="email"
+              name="email"
               placeholder="What's your email?"
               required
             />
             <input
               className="input message"
-              type="text"
+              type="name"
+              name="message"
               placeholder="What's your email?"
             />
             <div className="bg-light border btn-send">
-              <Button variant="primary">Send Message</Button>
+              <Button variant="primary" type="submit">
+                Send Message
+              </Button>
             </div>
           </form>
         </div>
